09755f57910baa4c3699bbae99b1abc9515e95f9
billfortuna <billjosephfortuna17@gmail.com>
2019-07-10 14:12:53 +0800

    Revert "Revert "My First Commit""
    This reverts commit de58bfa10e452d844e733833c8ffe87fab3d203e.

---
+++ store.js
@@ -0,0 +1,11 @@
+function showMenuList(id){
+			let divelement = document.getElementById(id);
+		
+			if(divelement.style.display == 'none')
+				divelement.style.display = 'block';
+				
+				
+			else
+			divelement.style.display="none";
+		alert('fuck');
+		}
